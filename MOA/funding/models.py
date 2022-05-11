ed834b12899cc899ca21067392c28bd3b5a506d2
KYBee <kyb3634@naver.com>
2022-05-11 22:28:33 +0900

    [create] fund models

--- MOA/funding/models.py
+++ MOA/funding/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from order.models import Order
+from user.models import Participant
 
 # Create your models here.
+class Fund(models.Model):
+    fund_id = models.PositiveIntegerField()
+    is_complete = models.BooleanField(default=False, verbose_name="완료여부")
+    created_at = models.DateField(auto_now_add=True, verbose_name="생성일")
+    updated_at = models.DateField(auto_now_add=True, verbose_name="수정일")
+    order_id = models.ForeinKey(Order, on_delete=models.SET_NULL, related_name="order_set")
+    participant_id = models.ForeinKey(Participant, on_delete=models.SET_NULL, related_name="participant_set")
