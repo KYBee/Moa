6b913c10a4c64f799bb094af691b3cd1d7570b3f
KYBee <kyb3634@naver.com>
2022-05-11 21:56:47 +0900

    [modify] : funding urls

--- MOA/funding/urls.py
+++ MOA/funding/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "funding"
 
 urlpatterns = [
-    path('/'),
+    path('read/<int:pk>/', funding_read, name = 'funding_read'),
+    path('create/', funding_create, name = 'funding_create'),
+    path('delete/<int:pk>', funding_delete, name = 'funding_delete'),
 ]
