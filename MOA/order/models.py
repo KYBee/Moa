6385475eb2d71c6846d5741c831df7372b942961
KYBee <kyb3634@naver.com>
2022-05-12 00:36:46 +0900

    [create] : order api

--- MOA/order/models.py
+++ MOA/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import Participant
 
-# Create your models here.
+class Order(models.Model):
+    store = models.CharField(max_length=30, verbose_name="상호")
+    receive_place = models.CharField(max_length=30, verbose_name="배송 희망 위치")
+    host = models.ForeinKey(Participant, on_delete=models.CASCADE, related_name="host_set", verbose_name="펀딩개설자", blank=True, null=True)
+    order_status = models.BooleanField(default=False, verbose_name="펀딩상태")
+    target_peope = models.PositiveIntegerField(verbose_name="펀딩 희망 인원")
+    target_money = models.PositiveIntegerField(verbose_name="펀딩 필요 금액")
+    target_time = models.DateTimeField(verbose_name="완료시간")
+    created_at = models.DateField(auto_now_add=True, verbose_name="생성일")
+    updated_at = models.DateField(auto_now_add=True, verbose_name="수정일")
+
+    def __str__(self):
+        return "{0} {1}".format(self.store, self.host)
--- MOA/order/views.py
+++ MOA/order/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import *
+from django.core.paginator import Paginator
 
-# Create your views here.
+def order_list(request):
+    orders = Order.objects.all()
+
+    #Pagination Code
+    context = {
+        'orders': orders
+    } 
+
+    return render(request, 'order/order_list.html', context)
+
+def order_read(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+
+    context = {
+        'order': order
+    }
+
+    return render(request, 'order/order_read.html', context)
+
+def order_create(request):
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            order = order.save()
+            order.host = request.user
+            order.save()
+
+            return redirect('order:order_read', order.pk)
+
+    else:
+        form = OrderForm()
+        context = {
+            'form' : form,
+        }
+        return render(request, 'order/order_create.html', context)
+
+
+def order_update(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.save()
+            return redirect('order:order_read', order.pk)
+        else:
+            return redirect('order:order_list')
+    else:
+        form = OrderForm(instance=order)
+        context = {
+            'form': form
+        }
+        return render(Order, 'order/order_update.html', context)
+
+
+def order_delete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+
+    if request.method == "POST":
+        order.delete()
+        return redirect('order:order_list')
+    else:
+        redirect('order:order_read', order.pk)
