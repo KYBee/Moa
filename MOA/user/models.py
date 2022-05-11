bb7b711566bcf0d96d59005c8f651ab90c2f1b7d
KYBee <kyb3634@naver.com>
2022-05-11 22:35:14 +0900

    [create] : user model

--- MOA/user/models.py
+++ MOA/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Participant(AbstractUser):
+    nickname = models.CharField(max_length=20, unique=True, verbose_name="닉네임")
+    email = models.EmailField(unique=True, verbose_name="이메일")
+    is_agree = models.BooleanField(default=False, verbose_name="약관동의")
+    created_at = models.DateField(auto_now_add=True, verbose_name="생성일")
+    updated_at = models.DateField(auto_now_add=True, verbose_name="수정일")
