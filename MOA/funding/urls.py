b279bc5d93eee36f1f7317521b7fff59645e811a
KYBee <kyb3634@naver.com>
2022-05-20 13:35:45 +0900

    [modify] : change url patterns

--- MOA/funding/urls.py
+++ MOA/funding/urls.py
@@ -5,7 +5,7 @@ app_name = "funding"
 
 urlpatterns = [
     # User 펀딩 목록 조회 : pk -> user.pk
-    path('list/<int:pk>/', funding_list, name = 'funding_list'),
+    path('list/<int:pk>', funding_list, name = 'funding_list'),
     # 펀딩 개설 : pk -> order.pk
     path('create/<int:pk>', funding_create, name = 'funding_create'),
     # 펀딩 취소 : pk -> funding.pk
--- MOA/user/urls.py
+++ MOA/user/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = "user"
 
 urlpatterns = [
-    path('login/', user_login, name='user_login'),
-    path('logout/', user_logout, name='user_logout'),
-    path('signup/', user_signup, name='user_signup'),
+    path('login', user_login, name='user_login'),
+    path('logout', user_logout, name='user_logout'),
+    path('signup', user_signup, name='user_signup'),
     #path('signup/success/', user_signup_success, name='user_signup_success'),
 
 ]
