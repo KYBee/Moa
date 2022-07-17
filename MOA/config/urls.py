cf1438ef2646443287d6a4195abfd6fd3c30db3a
KYBee <kyb3634@naver.com>
2022-07-17 18:41:53 +0900

    [modify] : config url

--- MOA/config/urls.py
+++ MOA/config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('order/', include('order.urls')),
     path('funding/', include('funding.urls')),
+    path('', lambda req:redirect('order:order_list'), name="home"),
+
 ]
