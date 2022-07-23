ff1e1519f4661e454bc079980eef85252a5f1807
KYBee <kyb3634@naver.com>
2022-07-23 16:08:48 +0900

    :lipstick: create order_read modals
    create order_read modals

--- MOA/config/static/js/modal.js
+++ MOA/config/static/js/modal.js
@@ -7,3 +7,53 @@ function show_modal() {
     const modal = document.getElementById('loggoutModal');
     modal.style = "display: block";
 }
+
+function close_update_modal() {
+    const modal = document.getElementById('updateOrderModal');
+    modal.style="display: none";
+}
+
+function show_update_modal() {
+    const modal = document.getElementById('updateOrderModal');
+    modal.style = "display: block";
+}
+
+function close_delete_modal() {
+    const modal = document.getElementById('deleteOrderModal');
+    modal.style="display: none";
+}
+
+function show_delete_modal() {
+    const modal = document.getElementById('deleteOrderModal');
+    modal.style = "display: block";
+}
+
+function close_complete_modal() {
+    const modal = document.getElementById('completeOrderModal');
+    modal.style="display: none";
+}
+
+function show_complete_modal() {
+    const modal = document.getElementById('completeOrderModal');
+    modal.style = "display: block";
+}
+
+function close_join_modal() {
+    const modal = document.getElementById('fundingCreateModal');
+    modal.style="display: none";
+}
+
+function show_join_modal() {
+    const modal = document.getElementById('fundingCreateModal');
+    modal.style = "display: block";
+}
+
+function close_join_delete_modal() {
+    const modal = document.getElementById('fundingDeleteModal');
+    modal.style="display: none";
+}
+
+function show_join_delete_modal() {
+    const modal = document.getElementById('fundingDeleteModal');
+    modal.style = "display: block";
+}
