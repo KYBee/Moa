ddc5e3da2a2ff1fb854f6b3fffe091bcd3c4af7b
KYBee <kyb3634@naver.com>
2022-07-23 21:46:16 +0900

    :art: refactor structure
    refactor structure

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +10,6 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('funding/', include('funding.urls')),
     path('', lambda req:redirect('order:order_list'), name="home"),
-
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
